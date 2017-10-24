ba6e6cd9db9038812361d8967fe4209cc7f696e6
raldo2 <raldo0104@gmail.com>
2017-10-25 01:05:50 +0700

    menu penjual jadi,

--- Soal1_penjual.c
+++ Soal1_penjual.c
@@ -12,40 +12,46 @@ pthread_t tid1,tid2;
 
 
 void *menu(void *argv){
-	int choice,jml;
+	int choice;
+	int jml;
 	char sjt[15];
 	while(1){
 		system("clear");
+		choice=0;
 		printf("1. Lihat Stock\n");
 		printf("2. Tambah Stock\n");
 		scanf("%d",&choice);
 		if(choice == 1){
-
+			system("clear");
+			printf("STOCK SENJATA\n\n");
+			printf("MP4A1 %d\n",*weapon[1]);
+			printf("PM2-V1 %d\n",*weapon[2]);
+			printf("SPR-3 %d\n",*weapon[3]);
+			printf("SS2-V5 %d\n",*weapon[4]);
+			printf("SPG1-V3 %d\n",*weapon[5]);
+			printf("MINE %d\n",*weapon[6]);
+			sleep(4);
 		}
 		else if(choice == 2){
 			printf("Masukkan nama senjata dan jumlah yang ditambah :\n");
-			scanf("%s %d", sjt  ,&jml);
-			char a[10]="MP4A1";
+			scanf("%s %d", sjt,&jml);
 			if((strcmp(sjt,"MP4A1") == 0)){
-					
-				printf("MP4A1anjing\n");
-				sleep(5);
-
+				*weapon[1]+=jml;
 			}
-			else if(sjt == "PM2-V1"){
-
+			else if((strcmp(sjt,"PM2-V1") == 0)){
+				*weapon[2]+=jml;
 			}
-			else if(sjt == "SPR-3"){
-
+			else if((strcmp(sjt,"SPR-3") == 0)){
+				*weapon[3]+=jml;
 			}
-			else if(sjt == "SS2-V5"){
-
+			else if((strcmp(sjt,"SS2-V5") == 0)){
+				*weapon[4]+=jml;
 			}
-			else if(sjt == "SPG1-V3"){
-
+			else if((strcmp(sjt,"SPG1-V3") == 0)){
+				*weapon[5]+=jml;
 			}
-			else if(sjt == "MINE"){
-
+			else if((strcmp(sjt,"MINE") == 0)){
+				*weapon[6]+=jml;
 			}
 			else printf("Senjata Tidak Ditemukan\n");
 		}
@@ -54,15 +60,24 @@ void *menu(void *argv){
 }
 
 int main(){
+	int i=0;
 	system("clear");
-	key_t key = 1234;
-	int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-	*weapon = shmat(shmid, NULL, 0);
-	//memset(*weapon, 0,sizeof(*weapon[7]));
+	key_t key[7];
+	for(i=0;i<=7;i++) {key[i]=i+20;}
+
+	int shmid[7];
+	for(i=0;i<=7;i++){
+		shmid[i] = shmget(key[i], sizeof(int), IPC_CREAT | 0666);
+	}
+	for(i=0;i<=7;i++){
+	weapon[i] = shmat(shmid[i], NULL, 0);
+	}
+
+	memset(*weapon, 0,sizeof(*weapon));
     pthread_create(&(tid1), NULL, &menu, NULL);
 
 	shmdt(weapon);
-    shmctl(shmid, IPC_RMID, NULL);
+	for(i=0;i<=7;i++) shmctl(shmid[i], IPC_RMID, NULL);
 
     pthread_join(tid1, NULL);
 }
