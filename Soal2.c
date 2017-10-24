0943d165c558ee53ee9000b8a87a14ef12ccdde3
raldo2 <raldo0104@gmail.com>
2017-10-25 04:05:52 +0700

    No 2 Fix jadi

--- Soal2.c
+++ Soal2.c
@@ -8,7 +8,7 @@
 int poin1,poin2;
 int lub1[17],lub2[17];
 char play1[50],play2[50];
-pthread_t tid1,tid2;
+pthread_t tid1,tid2,tid3;
 int ranj1,ranj2;
 int a,b,c,d;
 int d,e,f,g;
@@ -33,32 +33,33 @@ void * first(void *aww){
 			system("clear");
 			printf("%s Masukkan Jumlah Ranjau(Min 1, Max 4) = ",play1);
 			scanf("%d",&ranj1);
-			printf("Masukkan no lubang =  ");
+			printf("Masukkan no lubang(1-16) =  ");
 			if(ranj1==1){
 				scanf("%d",&a);
-				lub1[a]=1;
+				if(lub1[a]!=-1) lub1[a]=1;
 			}
 			else if(ranj1==2){
 				scanf("%d %d",&a,&b);
-				lub1[a]=1;
-				lub1[b]=1;
+				if(lub1[a]!=-1) lub1[a]=1;
+				if(lub1[b]!=-1) lub1[b]=1;
 			}
 			else if(ranj1==3){
 				scanf("%d %d %d",&a,&b,&c);
-				lub1[a]=1;
-				lub1[b]=1;
-				lub1[c]=1;
+				if(lub1[a]!=-1) lub1[a]=1;
+				if(lub1[b]!=-1) lub1[b]=1;
+				if(lub1[c]!=-1) lub1[c]=1;
 			}
 			else if(ranj1==4){
 				scanf("%d %d %d %d",&a,&b,&c,&d);
-				lub1[a]=1;
-				lub1[b]=1;
-				lub1[c]=1;
-				lub1[d]=1;
+				if(lub1[a]!=-1) lub1[a]=1;
+				if(lub1[b]!=-1) lub1[b]=1;
+				if(lub1[c]!=-1) lub1[c]=1;
+				if(lub1[d]!=-1) lub1[d]=1;
 			}
 			else printf("Ranjau minimal 1 maximal 4\n");
+			sleep(1);
 			system("clear");
-			printf("%s Menebak di Lubang = ", play2);
+			printf("%s Menebak 4 Lubang (1-16) = ", play2);
 			scanf("%d %d %d %d",&d,&e,&f,&g);
 			if(lub1[d]==1) {poin2+=1; lub1[d]= -1;}
 			if(lub1[d]==0) poin1+=1;
@@ -96,32 +97,33 @@ void * sec(void *arr){
 			system("clear");
 			printf("%s Masukkan Jumlah Ranjau(Min 1, Max 4) = ",play2);
 			scanf("%d",&ranj2);
-			printf("Masukkan no lubang =  ");
+			printf("Masukkan no lubang(1-16) =  ");
 			if(ranj2==1){
 				scanf("%d",&a);
-				lub2[a]=1;
+				if(lub2[a]!=-1) lub2[a]=1;
 			}
 			else if(ranj2==2){
 				scanf("%d %d",&a,&b);
-				lub2[a]=1;
-				lub2[b]=1;
+				if(lub2[a]!=-1) lub2[a]=1;
+				if(lub2[b]!=-1) lub2[b]=1;
 			}
 			else if(ranj1==3){
 				scanf("%d %d %d",&a,&b,&c);
-				lub2[a]=1;
-				lub2[b]=1;
-				lub2[c]=1;
+				if(lub2[a]!=-1) lub2[a]=1;
+				if(lub2[b]!=-1) lub2[b]=1;
+				if(lub2[c]!=-1) lub2[c]=1;
 			}
 			else if(ranj1==4){
 				scanf("%d %d %d %d",&a,&b,&c,&d);
-				lub2[a]=1;
-				lub2[b]=1;
-				lub2[c]=1;
-				lub2[d]=1;
+				if(lub2[a]!=-1) lub2[a]=1;
+				if(lub2[b]!=-1) lub2[b]=1;
+				if(lub2[c]!=-1) lub2[c]=1;
+				if(lub2[d]!=-1) lub2[d]=1;
 			}
 			else printf("Ranjau minimal 1 maximal 4\n");
+			sleep(1);
 			system("clear");
-			printf("%s Menebak di Lubang = ", play1);
+			printf("%s Menebak 4 Lubang(1-16) = ", play1);
 			scanf("%d %d %d %d",&d,&e,&f,&g);
 			if(lub2[d]==1) {poin1+=1; lub2[d]=-1;}
 			if(lub2[d]==0) poin2+=1;
@@ -138,6 +140,35 @@ void * sec(void *arr){
 }
 }
 
+void *check(void *cbc){
+	int i, check;
+	while(1){
+		int flag1=0,flag2=0;
+		for(i=1;i<17;i++){
+			if (lub1[i]==1 || lub1[i]==-1) flag1+=1;
+			if (lub2[i]==1 || lub2[i]==-1) flag2+=1;
+			if(flag1>=16 && flag2 >= 16){
+				sleep(0.8);
+				system("clear");
+				printf("Kedua Lubang telah Penuh Ranjau\n");
+				printf("Skor %s = %d\n", play1,poin1);
+				printf("Skor %s = %d\n", play2,poin2);
+				sleep(2);
+				exit(EXIT_SUCCESS);
+			}
+		}
+		if(poin2>=10 || poin1>=10){
+			printf("Game Berakhir\n");
+			printf("Skor %s = %d\n", play1,poin1);
+			printf("Skor %s = %d\n", play2,poin2);
+			sleep(2);
+			exit(EXIT_SUCCESS);
+		}
+	}
+}
+
+
+
 int main(){
 	system("clear");
 	printf("Nama Player 1 : ");
@@ -147,7 +178,10 @@ int main(){
 
 	pthread_create(&(tid1), NULL, &first, NULL);
 	pthread_create(&(tid2), NULL, &sec, NULL);
+	pthread_create(&(tid3), NULL, &check, NULL);
+    
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
 
 }
