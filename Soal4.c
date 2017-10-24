a2d8f34a308044232a994af1f30cbb7d641403b5
raldo2 <raldo0104@gmail.com>
2017-10-25 06:34:37 +0700

    No 4 alhmdulilah jadi

--- Soal4.c
+++ Soal4.c
@@ -5,16 +5,36 @@
 #include<unistd.h>
 #include<stdlib.h>
 
-pthread_t tid1;
+pthread_t tid[100];
 int arr[100];
+int status=0;
+int j=1;
+
+void *hitung(void * azz){
+	int fact=1;
+	int c;
+	for (c = 1; c <= arr[j]; c++) fact = fact * c;
+	printf("Hasil %d! = %d\n", arr[j],fact);
+	j++;
+	status=1;
+}
+
 
 int main(int argc , char **argv){
 	int a;
 	int i=1;
+	int k;
 	while(argv[i]!=NULL){
+		
 		arr[i]=atoi(argv[i]);
 		i++;
 	}
-	printf ("%d\n",arr[1]);
-
+	for(k=1;k<i;k++){
+	pthread_create(&(tid[k]), NULL, &hitung, NULL);
+	while(status==0){}
+	}
+		for(k=1;k<i;k++){
+	pthread_join(tid[k], NULL);
+	
+	}
 }
